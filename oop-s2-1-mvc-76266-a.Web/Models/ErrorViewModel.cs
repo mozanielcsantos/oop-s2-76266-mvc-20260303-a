85f59e20be198094cfa4242ab8d78c1c4d35354a
mozanielcsantos <mozanielcsantos@gmail.com>
2026-03-03 19:20:30 +0000

    Restart project from scratch (clean solution structure)

---
+++ oop-s2-1-mvc-76266-a.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace oop_s2_1_mvc_76266_a.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
